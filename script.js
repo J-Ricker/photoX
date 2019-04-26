b2ccacf97d3bade1647ae3f5f168b2b4f114b365
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-26 11:40:59 -0400

    added scroll animation script

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ $(document).ready(function() {
         } else {
             $('.nav-menu').removeClass('custom-navbar');
         }
-    })
+    });
+
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        if(position >= 650) {
+            $('.camera-img').addClass('fromLeft');
+            $('.mission-text').addClass('fromRight');
+        } else {
+            $('.camera-img').removeClass('fromLeft');
+            $('.mission-text').removeClass('fromLeft');
+        }
+    });
 });
 
 
